f686c70cbc4dd7f7b2448dd7b38f5dff0568d437
N1wan <hidden>
2025-06-24 11:19:52 +0200

    nvim turn off auto update checking

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -29,6 +29,4 @@ require("lazy").setup({
   },
   -- Configure any other settings here. See the documentation for more details.
   install = {},
-  -- automatically check for plugin updates
-  checker = { enabled = true },
 })
