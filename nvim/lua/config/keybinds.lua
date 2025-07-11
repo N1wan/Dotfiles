ac528d855aaaa928348bbaff98ce0f602c43ffe9
N1wan <hidden>
2025-07-11 15:37:22 +0200

    nvim keybinds add search clearing

--- nvim/lua/config/keybinds.lua
+++ nvim/lua/config/keybinds.lua
@@ -1,4 +1,9 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+-- open oil file explorer
 vim.keymap.set('n', '<leader>pf', ':Oil<Enter>', {})
+
+-- clear search with escape
+vim.keymap.set('n', '<esc>', ':noh<return><esc>', {})
+
