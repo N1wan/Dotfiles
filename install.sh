e0d31fabe09ce330419f9fa85440d32244f4e139
N1wan <niwan.b.v.h@gmail.com>
2025-09-21 18:42:17 +0200

    install.sh organize programs and add services and xorg

--- install.sh
+++ install.sh
@@ -33,6 +33,75 @@ else
     echo "[INFO] yay already installed."
 fi
 
+# -------------------------
+# categories
+# -------------------------
+
+DEVELOPMENT=(
+    java-environment-common java-runtime-common jdk-openjdk 
+    jdk21-openjdk maven nodejs npm python-pip ruby rust
+)
+
+SYSTEM=(
+    base base-devel amd-ucode bluez acpi alsa-utils alsa-plugins 
+    dhcpcd dosfstools e2fsprogs efibootmgr gnome-keyring grub 
+    linux linux-firmware linux-headers networkmanager ntp nvidia-open 
+    openssh optimus-manager-git os-prober pipewire pipewire-pulse 
+    qt5ct qt6ct sudo tlp tor wireplumber xdg-user-dirs xorg
+)
+
+TOOLS=(
+    bluez-utils arch-install-scripts clang dunst fzf luarocks 
+    man-db man-pages network-manager-applet pacman-contrib 
+    pamixer reflector scrot tree-sitter-cli unclutter upower
+
+    yay xclip zsh zoxide blueman batsignal bc brillo curl
+    fastfetch gdb git htop lazygit neovim pacseek qemu-full 
+    ripgrep timg tldr tmux tree unzip vi vim wget wine
+)
+
+PROGRAMS=(
+    arandr baobab discord dolphin gimp kitty libreoffice-fresh 
+    lutris nvidia-settings pavucontrol prismlauncher prismlauncher-themes-git 
+    python-conda python-eduvpn-client rofi signal-desktop steam 
+    thunderbird tigervnc torbrowser-launcher vlc zen-browser-bin
+    # quartus-free-devinfo-cyclone10lp quartus-free-quartus modelsim-intel-starter 
+    # intellij-idea-ultimate-edition
+)
+
+FONTS=(
+    noto-fonts noto-fonts-emoji papirus-icon-theme terminus-font
+    ttf-dejavu ttf-droid ttf-jetbrains-mono-nerd ttf-liberation
+    ttf-liberation-mono-nerd ttf-nerd-fonts-symbols-mono ttf-noto-nerd
+    ttf-roboto ttf-ubuntu-font-family
+)
+
+DISPLAY_MANAGERS=( 
+    lightdm lightdm-gtk-greeter lightdm-slick-greeter
+    # lightdm-webkit2-greeter lightdm-gtk-greeter-settings
+)
+
+WINDOW_MANAGERS=( 
+    i3-wm i3blocks 
+)
+
+# -------------------------
+# combine categories
+# -------------------------
+
+ALL_PACKAGES=(
+    "${PROGRAMS[@]}"
+    "${SYSTEM[@]}"
+    "${TOOLS[@]}"
+    "${FONTS[@]}"
+    "${DISPLAY_MANAGER[@]}"
+    "${WINDOW_MANAGER[@]}"
+)
+
+# -------------------------
+# filter + install
+# -------------------------
+
 check_packages() {
     local all_pkgs
     local valid_pkgs=()
@@ -49,70 +118,8 @@ check_packages() {
         fi
     done
 
-    # # log missing packages
-    # if ((${#missing_pkgs[@]} > 0)); then
-    #     printf '%s\n' "${missing_pkgs[@]}" | tee -a ~/missing_packages.log
-    # fi
-
     printf '%s\n' "${valid_pkgs[@]}"
 }
-
-# -------------------------
-# categories
-# -------------------------
-
-PROGRAMS=(
-  acpi alsa-plugins alsa-utils amd-ucode arandr arch-install-scripts baobab base base-devel
-  batsignal bc bind blueman bluez bluez-utils bottom brightnessctl brillo catppuccin-gtk-theme-mocha
-  clang composer curl dhcpcd dhcping dialog discord dmenu dmidecode dolphin dosfstools dunst
-  e2fsprogs efibootmgr fastfetch fd feh firefox fzf gdb gimp git gnome-keyring grub
-  haveged htop i3-wm i3blocks i3lock i3status imagemagick inetutils intellij-idea-ultimate-edition iw iwd
-  java-environment-common java-runtime-common jdk-openjdk jdk21-openjdk kitty lazygit
-  libreoffice-fresh lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings lightdm-slick-greeter
-  lightdm-webkit2-greeter linux linux-firmware linux-headers lshw luarocks lutris man-db man-pages maven
-  micromamba-bin mokutil msmtp msmtp-mta mtr neovim net-tools network-manager-applet
-  networkmanager nix nm-connection-editor nodejs nomacs npm ntfs-3g ntp nvidia-open nvidia-prime nvidia-settings
-  openssh optimus-manager-git os-prober pa-applet-git pacman-contrib pacseek pamixer pavucontrol perl-authen-sasl
-  perl-io-socket-ssl php pipewire pipewire-pulse powertop prismlauncher prismlauncher-themes-git
-  python-conda python-eduvpn-client python-pip python-pynvim qemu-full qt5ct qt6ct
-  quartus-free-devinfo-cyclone10lp quartus-free-quartus modelsim-intel-starter 
-  reflector ripgrep rofi ruby rust scrot
-  signal-desktop steam sudo texinfo thunderbird tigervnc timg tldr tlp tlp-rdw tmux tor torbrowser-launcher
-  tree tree-sitter-cli unclutter unzip upower vi vim vlc wget wine wireplumber wireshark-qt 
-  xclip 
-  xdg-user-dirs xorg-bdftopcf xorg-iceauth xorg-mkfontscale xorg-server xorg-sessreg xorg-setxkbmap xorg-smproxy
-  xorg-x11perf xorg-xbacklight xorg-xcmsdb xorg-xcursorgen xorg-xdpyinfo xorg-xdriinfo xorg-xev xorg-xgamma
-  xorg-xhost xorg-xinit xorg-xinput xorg-xkbcomp xorg-xkbevd xorg-xkbprint xorg-xkbutils xorg-xkill
-  xorg-xlsatoms xorg-xlsclients xorg-xpr xorg-xrandr xorg-xrefresh xorg-xset xorg-xsetroot xorg-xvinfo
-  xorg-xwd xorg-xwininfo xorg-xwud 
-  yay zen-browser-bin zoxide zsh
-)
-
-FONTS=(
-  noto-fonts noto-fonts-emoji papirus-icon-theme terminus-font
-  ttf-dejavu ttf-droid ttf-jetbrains-mono-nerd ttf-liberation
-  ttf-liberation-mono-nerd ttf-nerd-fonts-symbols-mono ttf-noto-nerd
-  ttf-roboto ttf-ubuntu-font-family
-)
-
-# You can add more categories here:
-# DEV_TOOLS=( docker docker-compose postgresql )
-# GAMES=( minecraft-launcher heroic-games-launcher )
-
-# -------------------------
-# combine categories
-# -------------------------
-
-ALL_PACKAGES=(
-  "${PROGRAMS[@]}"
-  "${FONTS[@]}"
-  # "${DEV_TOOLS[@]}"
-  # "${GAMES[@]}"
-)
-
-# -------------------------
-# filter + install
-# -------------------------
 
 VALID_PACKAGES=($(check_packages "${ALL_PACKAGES[@]}"))
 
@@ -140,6 +147,7 @@ else
 fi
 
 # symlinking config files
+ln -sfn ~/Dotfiles/xorg/xprofile ~/.xprofile
 ln -sfn ~/Dotfiles/zsh/zshrc ~/.zshrc
 ln -sfn ~/Dotfiles/zsh/p10k.zsh ~/.p10k.zsh
 ln -sfn ~/Dotfiles/rofi ~/.config/rofi
@@ -180,5 +188,10 @@ enable_grub_theme() {
 enable_grub_theme
 
 # enabling services
+sudo systemctl enable --now tlp.service
+sudo systemctl mask systemd-rfkill.service
+sudo systemctl mask systemd-rfkill.socket
 sudo systemctl enable --now NetworkManager.service
+sudo systemctl enable --now --user wireplumber.service
+sudo systemctl enable --now ntpd.service
 sudo systemctl enable --now lightdm.service
