770dd0bea644530781d73569f8b75144eaa33b46
N1wan <niwan.b.v.h@gmail.com>
2025-10-24 10:20:36 +0200

    nvim add auto cmd for javascript and typescript tabstop

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -4,3 +4,13 @@ vim.api.nvim_create_autocmd("FileType", {
 		dofile(vim.fn.stdpath("config") .. "/lua/config/options.lua")
 	end,
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
+  callback = function()
+    vim.bo.tabstop = 2
+    vim.bo.shiftwidth = 2
+    vim.bo.expandtab = true
+  end,
+})
+
