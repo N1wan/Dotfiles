560470da63f1ddd67297145a379a1de37be72dc7
Niwan van den Berch van Heemstede <niwan.b.v.h@gmail.com>
2026-05-12 15:17:13 +0200

    i3 fix bar (gets rid of starting in toggle mode :/ ill fix it later)

--- i3/scripts/battery_status.sh
+++ i3/scripts/battery_status.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
-# Get status and percentage from upower
-info=$(upower -i $(upower -e | grep BAT) | grep -E "state|percentage")
+# Try to find any BAT device
+batt_path=$(upower -e 2>/dev/null | grep -i bat | head -1)
 
+if [ -z "$batt_path" ]; then
+    # No battery found (desktop or broken detection)
+    echo "N/A"
+    exit 0
+fi
+
+info=$(upower -i "$batt_path" 2>/dev/null | grep -E "state|percentage")
 status=$(echo "$info" | grep state | awk '{print $2}')
 percent=$(echo "$info" | grep percentage | awk '{print $2}' | sed 's/%//')
 
-# Set icon
 if [[ $status == "charging" ]]; then
     icon="⚡ "
 else
     icon=""
 fi
 
-echo "$icon$percent%"
+echo "${icon}${percent}%"
+exit 0
 
